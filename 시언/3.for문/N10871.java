6fc3880b2dee7bf594b36ff1ccc13dfc4e8f24bb
josieon <76546008+josieon@users.noreply.github.com>
2022-02-18 12:49:32 +0900

    Update N10871.java

--- 시언/3.for문/N10871.java
+++ 시언/3.for문/N10871.java
@@ -19,11 +19,15 @@ public class N10871 {
 		}
 		bw.flush();
 	}
-/* �� ������ Ǯ�鼭 StringTokenizer�� ����Ͽ��µ�
- * StringTokenizer�� ���� ���е� String�� TypeCasting�ϴ� �Ͱ�
- * �� ���� ���ڿ��� �޾ƿ��ٺ��� BufferedReader�� readLine�� �� �� ȣ�����ְ� �Ǵµ�
- * �� ������ StringTokenzier�� �����ڸ� �ٽ� ȣ�����־�� �ϴ� ���� ���ŷο���.
- * StringTokenizer�� ���� �� �� �����غ� �ʿ䰡 �ִٰ� �����߰�
- * �ٸ� ����� �ڵ带 ���� �����ȿ� ���� �н��ؾ߰ڴ�.
+/* 본 문제를 풀면서 StringTokenizer를 사용하였는데
+ * StringTokenizer를 통해 구분된 String을 TypeCasting하는 것과
+ * 두 행의 문자열을 받아오다보니 BufferedReader의 readLine을 두 번 호출해주게 되는데
+ * 그 때마다 StringTokenzier의 생성자를 다시 호출해주어야 하는 점이 번거로웠다.
+ * StringTokenizer에 대해 좀 더 공부해볼 필요가 있다고 생각했고
+ * 다른 사람의 코드를 보며 개선안에 대해 학습해야겠다.
+ */
+/* 다른 사람들의 풀이를 보며 공부를 하였는데 내가 풀었던 방식이
+ * 효율적인 방식이라고 소개가 되고 있다. 대신에 나는 readLine을 호출해서 변수에 값을 주는데 이를 생략하고
+ * StringTokenizer 생성자의 인자로 readLine을 호출하면 실행시간과 메모리를 더 줄일 수 있다.
  */
 }
