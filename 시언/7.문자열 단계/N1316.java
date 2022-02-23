e348adcdd9f65b49792d1aba8f7d787812bb1632
josieon <76546008+josieon@users.noreply.github.com>
2022-02-23 17:03:51 +0900

    Update N1316.java

--- 시언/7.문자열 단계/N1316.java
+++ 시언/7.문자열 단계/N1316.java
@@ -15,7 +15,7 @@ public class N1316 {
 	public static boolean checker(String str) {
 		int[] idx = new int[26];
 		String arr=new String();
-		for(int i=0; i<str.length(); i++) {
+		for(int i=0; i<str.length(); i++) {	//문자열을 압축하는 반복문
 			if(i==0)
 				arr = arr + str.charAt(i);
 			else {
@@ -23,7 +23,7 @@ public class N1316 {
 					arr = arr + str.charAt(i);
 			}
 		}
-		for(int i=0; i<arr.length(); i++) {
+		for(int i=0; i<arr.length(); i++) {	//압축된 문자열을 참조해 그룹 단어인지 여부 판별
 			if(idx[(int)arr.charAt(i)-97]==0)
 				idx[(int)arr.charAt(i)-97]=1;
 			else
@@ -33,3 +33,5 @@ public class N1316 {
 	}
 
 }
+//위와 같이 푼 게 내가 푼 방식이고 검색을 해보니 문자열을 압축할 필요없이 if문을 사용해 previous 문자와 현재 문자를 가지고 할 수 있다.
+//prev과 now가 다르다면 배열의 값을 참조해 true false를 판별 가능. prev과 now가 같다면 loop continue
